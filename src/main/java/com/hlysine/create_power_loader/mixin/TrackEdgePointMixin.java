905ccab11c63dc1b8db443890fa9202382305072
Henry Lin <henry.ys.lin@gmail.com>
2024-01-06 20:57:03 +0800

    Add NotNull annotation

--- src/main/java/com/hlysine/create_power_loader/mixin/TrackEdgePointMixin.java
+++ src/main/java/com/hlysine/create_power_loader/mixin/TrackEdgePointMixin.java
@@ -1,10 +1,8 @@
 package com.hlysine.create_power_loader.mixin;
 
 import com.hlysine.create_power_loader.content.trains.CPLGlobalStation;
-import com.hlysine.create_power_loader.content.trains.StationChunkLoader;
 import com.simibubi.create.content.trains.graph.TrackGraph;
 import com.simibubi.create.content.trains.signal.TrackEdgePoint;
-import com.simibubi.create.content.trains.station.GlobalStation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,9 +16,6 @@ public class TrackEdgePointMixin {
     )
     public void cpl$tick(TrackGraph graph, boolean preTrains, CallbackInfo ci) {
         if (this instanceof CPLGlobalStation station) {
-            if (station.getLoader() == null)
-                //noinspection DataFlowIssue
-                station.setLoader(new StationChunkLoader((GlobalStation) station));
             station.getLoader().tick(graph, preTrains);
         }
     }
