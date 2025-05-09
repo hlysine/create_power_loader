c9053cd7a77d9a57e053db1abc281ae7ff2346c1
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2025-05-09 02:11:06 +0100

    Bring buildscript up to 1.21.1 neoforge

--- src/main/java/com/hlysine/create_power_loader/CreatePowerLoader.java
+++ src/main/java/com/hlysine/create_power_loader/CreatePowerLoader.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CreatePowerLoader.MODID)
 public class CreatePowerLoader {
     // Define mod id in a common place for everything to reference
