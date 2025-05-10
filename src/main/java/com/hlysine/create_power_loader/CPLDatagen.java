9f76f1355b5d95e9bf2267b60114423b2b04c935
Henry Lin <henry.ys.lin@gmail.com>
2025-05-10 11:59:41 +0800

    Fix datagen

--- src/main/java/com/hlysine/create_power_loader/CPLDatagen.java
+++ src/main/java/com/hlysine/create_power_loader/CPLDatagen.java
@@ -1,6 +1,7 @@
 package com.hlysine.create_power_loader;
 
 import com.tterrag.registrate.providers.ProviderType;
+import com.tterrag.registrate.providers.RegistrateDataProvider;
 import net.minecraft.tags.TagEntry;
 import net.minecraft.world.entity.EntityType;
 import com.tterrag.registrate.providers.RegistrateTagsProvider;
@@ -10,6 +11,8 @@ import net.neoforged.neoforge.data.event.GatherDataEvent;
 public class CPLDatagen {
     public static void gatherData(GatherDataEvent event) {
         CreatePowerLoader.getRegistrate().addDataGenerator(ProviderType.ENTITY_TAGS, CPLDatagen::genEntityTags);
+        // todo: this line shouldn't be necessary because registrate should add the data provider itself
+        event.getGenerator().addProvider(true, CreatePowerLoader.getRegistrate().setDataProvider(new RegistrateDataProvider(CreatePowerLoader.getRegistrate(), CreatePowerLoader.MODID, event)));
     }
 
     private static void genEntityTags(RegistrateTagsProvider<EntityType<?>> prov) {
